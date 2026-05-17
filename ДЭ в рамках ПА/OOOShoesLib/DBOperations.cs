65902925c78cebe01f94dbdde73336fae4cd2c8c
Anon1m4k <kirilvalera2@gmail.com>
2026-05-17 19:52:08 +0300

    Ооо...

--- ДЭ в рамках ПА/OOOShoesLib/DBOperations.cs
+++ ДЭ в рамках ПА/OOOShoesLib/DBOperations.cs
@@ -15,7 +15,8 @@ namespace OOOShoesLib
             using (var conn = new MySqlConnection(ConnectionString))
             {
                 conn.Open();
-                string query = "SELECT id, full_name, role, login, password FROM users WHERE login = @login AND password = @password";
+                //string query = "SELECT id, full_name, role, login, password FROM users WHERE login = @login AND password = @password";
+                string query = "SELECT * FROM users WHERE Логин = @login AND Пароль = @password";
                 var cmd = new MySqlCommand(query, conn);
                 cmd.Parameters.AddWithValue("@login", login);
                 cmd.Parameters.AddWithValue("@password", password);
@@ -25,11 +26,17 @@ namespace OOOShoesLib
                     {
                         return new Users
                         {
-                            Id = reader.GetInt32("id"),
+                            /*Id = reader.GetInt32("id"),
                             FullName = reader.GetString("full_name"),                            
                             Role = reader.GetString("role"),
                             Login = reader.GetString("login"),
-                            Password = reader.GetString("password")
+                            Password = reader.GetString("password")*/
+
+                            Id = reader.GetInt32("Id"),
+                            FullName = reader.GetString("ФИО"),
+                            Role = reader.GetString("Роль сотрудника"),
+                            Login = reader.GetString("Логин"),
+                            Password = reader.GetString("Пароль")
                         };
                     }
                 }
@@ -44,7 +51,8 @@ namespace OOOShoesLib
             using (var conn = new MySqlConnection(ConnectionString))
             {
                 conn.Open();
-                string query = "SELECT article, name, unit, price, supplier, manufacturer, category, discount, quantity, description, photo FROM products";
+                //string query = "SELECT article, name, unit, price, supplier, manufacturer, category, discount, quantity, description, photo FROM products";
+                string query = "SELECT * FROM products";
                 var cmd = new MySqlCommand(query, conn);
                 using (var reader = cmd.ExecuteReader())
                 {
@@ -52,7 +60,7 @@ namespace OOOShoesLib
                     {
                         products.Add(new Products
                         {
-                            Article = reader.GetString("article"),
+                            /*Article = reader.GetString("article"),
                             Name = reader.GetString("name"),
                             Unit = reader.GetString("unit"),
                             Price = reader.GetDecimal("price"),
@@ -62,7 +70,19 @@ namespace OOOShoesLib
                             Discount = reader.GetInt32("discount"),
                             Quantity = reader.GetInt32("quantity"),
                             Description = reader.GetString("description"),
-                            Photo =  reader.GetString("photo")
+                            Photo =  reader.GetString("photo")*/
+
+                            Article = reader.GetString("Артикул"),
+                            Name = reader.GetString("Наименование товара"),
+                            Unit = reader.GetString("Единица измерения"),
+                            Price = reader.GetDecimal("Цена"),
+                            Supplier = reader.GetString("Поставщик"),
+                            Manufacturer = reader.GetString("Производитель"),
+                            Category = reader.GetString("Категория товара"),
+                            Discount = reader.GetInt32("Действующая скидка"),
+                            Quantity = reader.GetInt32("Кол-во на складе"),
+                            Description = reader.GetString("Описание товара"),
+                            Photo = reader.GetString("Фото")
                         });
                     }
                 }
